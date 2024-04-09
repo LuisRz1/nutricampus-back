341e9c6e28ce577268209fc7e166ce5b6de76080
jmlc643 <josemarialuyocampos@gmail.com>
2024-04-08 22:20:21 -0500

    first commit

---
+++ src/main/java/com/upao/pe/nutricampus/NutricampusApplication.java
@@ -0,0 +1,13 @@
+package com.upao.pe.nutricampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricampusApplication.class, args);
+	}
+
+}
